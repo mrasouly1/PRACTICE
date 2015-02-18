59d93e0734429983841747b0d840c9e7cc4cccef
class <class@CST_N418_18.cst.citytech.cuny.edu>
2015-02-18 13:39:17 -0500

    PRACTICE

---
+++ src/counter/Counter.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package counter;
+
+/**
+ *
+ * @author class
+ */
+public class Counter {
+    int mycount = 0;
+    static int ourcount = 0;
+    
+    void increment(){
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Counter counter = new counter();
+        Counter counter = new counter();
+        
+        
+        System.out.println("Counter 1: " + Counter 1.myCount +
+               
+                " " + Counter.ourcount);
+    }
+    
+}
